4f02ab81644e26b86152c6e3b3fab971f951fdc7
system1 <braungoodson@gmail.com>
2014-04-16 19:55:27 -0400

    Update dev-stack.sh

--- dev-stack.sh
+++ dev-stack.sh
@@ -9,18 +9,12 @@ echo ""
 sudo mkdir -p /data/db
 
 sudo apt-get -y update
-sudo apt-get -y install zsh
 sudo apt-get -y install make
 sudo apt-get -y install g++
 sudo apt-get -y install git
 sudo apt-get -y install mongodb
 curl -O http://nodejs.org/dist/v0.10.24/node-v0.10.24-linux-x64.tar.gz
 tar -zxvf node-v0.10.24-linux-x64.tar.gz
-
-echo "# chsh override" >> ~/.bashrc
-echo "zsh" >> ~/.bashrc
-
-curl https://raw.github.com/braungoodson/dev-stack/master/.zshrc >> ~/.zshrc
 
 echo "\033[0;34m"'      .___                              __                 __    '"\033[0m"
 echo "\033[0;34m"'    __| _/_______  __           _______/  |______    ____ |  | __'"\033[0m"
@@ -29,3 +23,4 @@ echo "\033[0;34m"'  / /_/ \  ___/\   /  /_____/  \___ \  |  |  / __ \\  \___|   
 echo "\033[0;34m"'  \____ |\___  >\_/           /____  > |__| (____  /\___  >__|_ \ '"\033[0m"
 echo "\033[0;34m"'       \/    \/                    \/            \/     \/     \/'"\033[0m"
 echo ""
+echo "\t\tmake,g++,git,mongodb,nodejs"
